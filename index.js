35077a4ab80372967bc844ad837ef64e76c7801e
unknown <maybex66@gmail.com>
2022-06-21 11:54:58 -0400

    output root

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ let whitelistAddresses = require("./whitelist.json")
 const leafNodes = whitelistAddresses.map(addr => keccak256(addr))
 const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true })
 
+
 console.log(merkleTree.toString())
+console.log("Root: ", merkleTree.getRoot().toString('hex'))
